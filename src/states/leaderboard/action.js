87226e0bf07bdc5eadfb90c40daf82a7737c59b9
Nisrina <nisrinazahran127@gmail.com>
2024-05-19 12:55:45 +0700

    update hooks

--- src/states/leaderboard/action.js
+++ src/states/leaderboard/action.js
@@ -1,9 +1,9 @@
 import { hideLoading, showLoading } from 'react-redux-loading-bar';
 import api from '../../utils/api';
 
-// const ActionType = {
-//   RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS',
-// };
+const ActionType = {
+  RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS',
+};
 
 function receiveLeaderboardsActionCreator(leaderboards) {
   return {
